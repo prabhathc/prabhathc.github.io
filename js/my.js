3af9d2b85c3bf73a05ac4dd6fe276d0b1f13371e
unknown <prabhathchandanala22@gmail.com>
2018-09-27 15:08:32 -0400

    second commit

---
+++ js/my.js
@@ -0,0 +1,14 @@
+document.getElementsById('resume')[0]
+        .addEventListener('click', function (event) {
+            window.alert('resume');
+        });
+
+document.getElementsById('github')[0]
+        .addEventListener('click', function (event) {
+            window.alert('github');
+        });
+
+document.getElementsById('linkedin')[0]
+        .addEventListener('click', function (event) {
+            window.alert('linkedin');
+        });
