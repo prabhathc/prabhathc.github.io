1023efc455dac133c38640625a2286924d274586
unknown <prabhathchandanala22@gmail.com>
2018-09-29 21:27:09 -0400

    added copy-email capability

--- js/my.js
+++ js/my.js
@@ -1,14 +1,6 @@
-document.getElementsById('resume')[0]
-        .addEventListener('click', function (event) {
-            window.alert('resume');
-        });
-
-document.getElementsById('github')[0]
-        .addEventListener('click', function (event) {
-            window.alert('github');
-        });
-
-document.getElementsById('linkedin')[0]
-        .addEventListener('click', function (event) {
-            window.alert('linkedin');
-        });
+function copy() {
+    var copytext = "prabhathc88@gmail.com";
+    copytext.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
