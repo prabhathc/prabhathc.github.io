74e0b8a09d1a88164de35942d1d33c42d925ddad
prabhathc <17132210+prabhathc@users.noreply.github.com>
2023-06-21 23:25:45 -0400

    feature(updated resume/email

--- js/my.js
+++ js/my.js
@@ -1,5 +1,5 @@
 function copy() {
-    var copytext = "prabhathc88@gmail.com";
+    var copytext = "prabhathc@icloud.com";
     copytext.select();
     document.execCommand("copy");
     alert("Copied the text: " + copyText.value);
